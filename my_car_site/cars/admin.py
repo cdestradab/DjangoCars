41655d05dcf9e8d3b3e8aa24d0194fc6d2a4c8d0
cdestradab <cdestradab@gmail.com>
2023-03-16 00:12:29 -0500

    fields for CarAdmin DONE

--- my_car_site/cars/admin.py
+++ my_car_site/cars/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
-
+from cars.models import Car
 # Register your models here.
+
+class CarAdmin(admin.ModelAdmin):
+    fields = ['year', 'brand']
+
+admin.site.register(Car, CarAdmin)
